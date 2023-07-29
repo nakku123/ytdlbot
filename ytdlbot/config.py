e773ab1b3a89a42f9f3aa926a0a36f8d83645fac
mithun <130681565+nakku123@users.noreply.github.com>
2023-07-29 22:21:54 +0530

    config.py

--- ytdlbot/config.py
+++ ytdlbot/config.py
@@ -12,9 +12,9 @@ import os
 # general settings
 WORKERS: int = int(os.getenv("WORKERS", 100))
 PYRO_WORKERS: int = int(os.getenv("PYRO_WORKERS", min(64, (os.cpu_count() + 4) * 10)))
-APP_ID: int = int(os.getenv("APP_ID", 198214))
-APP_HASH = os.getenv("APP_HASH", "1234b90")
-TOKEN = os.getenv("TOKEN", "1234")
+APP_ID: int = int(os.getenv("APP_ID", 9439609))
+APP_HASH = os.getenv("APP_HASH", "3a64962f1face2fc285d0bb72587b139")
+TOKEN = os.getenv("TOKEN", "6483839103:AAHoplxNaqnXnVdOJ9vJ5lC8f_7hiaZRi0k")
 
 REDIS = os.getenv("REDIS", "redis")
 
@@ -24,18 +24,18 @@ TG_MAX_SIZE = 2000 * 1024 * 1024
 EXPIRE = 24 * 3600
 
 ENABLE_VIP = os.getenv("VIP", False)
-AFD_LINK = os.getenv("AFD_LINK", "https://afdian.net/@BennyThink")
-COFFEE_LINK = os.getenv("COFFEE_LINK", "https://www.buymeacoffee.com/bennythink")
+AFD_LINK = os.getenv("AFD_LINK", "https://t.me/mithun_adminbot")
+COFFEE_LINK = os.getenv("COFFEE_LINK", "https://t.me/mithun_adminbot")
 COFFEE_TOKEN = os.getenv("COFFEE_TOKEN")
 AFD_TOKEN = os.getenv("AFD_TOKEN")
 AFD_USER_ID = os.getenv("AFD_USER_ID")
-OWNER = os.getenv("OWNER", "BennyThink")
+OWNER = os.getenv("OWNER", "MemesMithun")
 
 # limitation settings
-AUTHORIZED_USER: str = os.getenv("AUTHORIZED_USER", "")
+AUTHORIZED_USER: str = os.getenv("AUTHORIZED_USER", "MemesMithun")
 # membership requires: the format could be username(without @ sign)/chat_id of channel or group.
 # You need to add the bot to this group/channel as admin
-REQUIRED_MEMBERSHIP: str = os.getenv("REQUIRED_MEMBERSHIP", "")
+REQUIRED_MEMBERSHIP: str = os.getenv("REQUIRED_MEMBERSHIP", "englishkaaranmain")
 
 # celery related
 ENABLE_CELERY = os.getenv("ENABLE_CELERY", False)
@@ -58,10 +58,10 @@ PROVIDER_TOKEN = os.getenv("PROVIDER_TOKEN") or "1234"
 PLAYLIST_SUPPORT = os.getenv("PLAYLIST_SUPPORT", False)
 ENABLE_ARIA2 = os.getenv("ENABLE_ARIA2", False)
 
-FREE_DOWNLOAD = os.getenv("FREE_DOWNLOAD", 20)
+FREE_DOWNLOAD = os.getenv("FREE_DOWNLOAD", 50)
 TOKEN_PRICE = os.getenv("BUY_UNIT", 20)  # one USD=20 credits
 
-RATE_LIMIT = os.getenv("RATE_LIMIT", 20)
+RATE_LIMIT = os.getenv("RATE_LIMIT", 50)
 
 DSN = os.getenv("DSN")
 SS_YOUTUBE = os.getenv("SS_YOUTUBE", "https://ytdlbot.dmesg.app?token=123456")
